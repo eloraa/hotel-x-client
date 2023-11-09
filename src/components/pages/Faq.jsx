d31d9168dccaccbff3c3bc66863ee6125b96a47d
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-09 13:56:53 +0000

    highlight brand in faq

--- src/components/pages/Faq.jsx
+++ src/components/pages/Faq.jsx
@@ -25,7 +25,9 @@ export const Faq = () => {
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>Is breakfast included in the room rate?</h1>
-          <p className="mt-6 font-semibold">Breakfast inclusion varies by room type and package. You can check the details while booking your room, or feel free to contact our front desk for clarification</p>
+          <p className="mt-6 font-semibold">
+            Breakfast inclusion varies by room type and package. You can check the details while booking your room, or feel free to contact our front desk for clarification
+          </p>
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>What time is check-in and check-out?</h1>
@@ -44,7 +46,7 @@ export const Faq = () => {
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>Does Hotel have on-site dining options?</h1>
-          <p className="mt-6 font-semibold">Absolutely! We have Foody, which serves delicious cuisine. Our bar, Vintage, is also a great place to relax and unwind.</p>
+          <p className="mt-6 font-semibold">Absolutely! We have <b>Foody</b>, which serves delicious cuisine. Our bar, <b>Vintage</b>, is also a great place to relax and unwind.</p>
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>Can I bring my pet to Hotel?</h1>
@@ -52,7 +54,9 @@ export const Faq = () => {
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>How can I get to local attractions from Hotel?</h1>
-          <p className="mt-6 font-semibold">Our concierge can assist you in arranging transportation or providing directions to popular local attractions. We&apos;re here to help you make the most of your stay.</p>
+          <p className="mt-6 font-semibold">
+            Our concierge can assist you in arranging transportation or providing directions to popular local attractions. We&apos;re here to help you make the most of your stay.
+          </p>
         </div>
         <div className="px-8 py-14 bg-[#f4f4f4] rounded-lg">
           <h1>Do you offer special rates for group bookings or events?</h1>
